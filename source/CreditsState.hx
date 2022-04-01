d4e1482da543eb27bc6f9fe3833b672d0f822916
Iamsamsmich6 <99293135+Iamsamsmich6@users.noreply.github.com>
2022-04-01 20:21:07 -0300

    CreditsState.hx

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -48,6 +48,7 @@ class CreditsState extends MusicBeatState
 		var pisspoop = [ //Name - Icon name - Description - Link - BG Color
 			['Psych Engine Android Port'],
 			['M.A. Jigsaw',		    'majigsaw',		    'Main Coder of The Port',	 'https://www.youtube.com/channel/UC2Sk7vtPzOvbVzdVTWrribQ',	'0xFFC30085'],
+			['loona and pico',		    'loona',		    'psych custom coder',	'https://youtube.com/channel/UCOHxtONBqAC6m7RWOh5OGw', 	'0xFFC30085
 			['Psych Engine Team'],
 			['Shadow Mario',		'shadowmario',		'Main Programmer of Psych Engine',					'https://twitter.com/Shadow_Mario_',	'0xFFFFDD33'],
 			['RiverOaken',			'riveroaken',		'Main Artist/Animator of Psych Engine',				'https://twitter.com/river_oaken',		'0xFFC30085'],
